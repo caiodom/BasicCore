81f1e26f360e0f20dd3c9721cb9f131992215b3f
Caio Henrique Domingues Leite <caio_henrrique@hotmail.com>
2021-09-18 17:21:22 -0300

    responses

---
+++ Core/Communication/ResponseErrorMessages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Communication
+{
+    public class ResponseErrorMessages
+    {
+
+        public List<string> Messages { get; set; }
+        public ResponseErrorMessages()
+        {
+            Messages = new List<string>();
+        }
+
+        
+
+    }
+}
---
+++ Core/Communication/ResponseResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Communication
+{
+   public class ResponseResult
+    {
+
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public ResponseErrorMessages Errors { get; set; }
+        
+
+        public ResponseResult()
+        {
+            Errors = new ResponseErrorMessages();
+        }
+
+        
+    }
+}
